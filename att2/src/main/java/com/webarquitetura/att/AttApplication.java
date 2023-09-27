621ae647396d6a09fb48c57681c0c13a2e022650
Vinicius Domingues Silva <91794441+ViniciusDSS@users.noreply.github.com>
2023-09-27 16:26:41 -0300

    att

--- att2/src/main/java/com/webarquitetura/att/AttApplication.java
+++ att2/src/main/java/com/webarquitetura/att/AttApplication.java
@@ -24,6 +24,7 @@ public class AttApplication {
             repository.findByNome("relogio");
 
             caterepository.findByNome("relogio");
+            caterepository.findByIdWithProduto((long) 2);
 
         };
     }
--- att2/src/main/java/com/webarquitetura/att/repository/CategoriaProdutoRepository.java
+++ att2/src/main/java/com/webarquitetura/att/repository/CategoriaProdutoRepository.java
@@ -1,8 +1,11 @@
 package com.webarquitetura.att.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.webarquitetura.att.models.Categoria;
@@ -12,4 +15,7 @@ public interface CategoriaProdutoRepository extends JpaRepository<Categoria, Lon
 
 
     List<Categoria> findByNome(String nome);
+
+    @Query("SELECT c FROM Categoria c LEFT JOIN FETCH c.produtos WHERE c.id_categoria = :id_categoria")
+    Optional<Categoria> findByIdWithProduto(@Param("id_categoria") Long id_categoria);
 }
